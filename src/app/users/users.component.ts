37034e694ff4a089d1b1d96c68ac7d32674e9d3c
Halil Bertan ÇAKICI <bertancakici@gmail.com>
2018-09-06 23:30:02 +0300

    a

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -6,23 +6,33 @@ import {UserServiceService} from './user-service.service';
     selector: 'app-users',
     templateUrl: './users.component.html',
     styleUrls: ['./users.component.css'],
-    providers:[UserServiceService]
+    providers: [UserServiceService]
 })
 export class UsersComponent implements OnInit {
 
     users: User[];
 
+    /*interval=0;*/
+
     constructor(private userService: UserServiceService) {
     }
 
+
     ngOnInit() {
-        this.getUsers();
+
+       this.getUsers();
+        /*this.interval = setInterval(() => {
+             this.getUsers();
+         }, 1000);*/
+        //this.getUsers();
     }
 
     getUsers() {
         this.userService.getUsers().subscribe(res => {
-                this.users = res
-            });
+            this.users = res;
+        });
     }
+
+
 
 }
